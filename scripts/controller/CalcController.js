05b74a60a600bb1bd1bde019ae0e98f294b05720
Fábio Amorim <141679031+fabiofamorim@users.noreply.github.com>
2023-12-08 13:32:30 -0300

    Calculadora finalizada
    Ultimas atualizações:
    limite de 10 dig no display,
    visualização de erro no display e try catch.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,8 @@ class CalcController {
 
     constructor(){
 
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
 
@@ -13,6 +15,35 @@ class CalcController {
         this._currentDate;
         this.initialize();
         this.initButtonsEvents();
+        this.initKeyboard();
+
+    }
+
+    pasteFromClipboard(){
+
+        document.addEventListener('paste', e=>{
+
+            let text = e.clipboardData.getData('Text');
+
+            this.displayCalc = parseFloat(text);
+
+        });
+
+    }
+
+    copyToClipboard(){
+
+        let input = document.createElement('input');
+
+        input.value = this.displayCalc;
+
+        document.body.appendChild(input);
+
+        input.select();
+
+        document.execCommand("Copy");
+
+        input.remove();
 
     }
 
@@ -27,6 +58,91 @@ class CalcController {
         }, 1000);
 
         this.setLastNumberToDisplay();
+        this.pasteFromClipboard();
+
+        document.querySelectorAll('.btn-ac').forEach(btn=>{
+
+            btn.addEventListener('dblclick', e=>{
+
+                this.toggleAudio();
+
+            });
+
+        });
+
+    }
+
+    toggleAudio(){
+
+        this._audioOnOff = !this._audioOnOff;
+
+    }
+
+    playAudio(){
+
+        if (this._audioOnOff) {
+
+            this._audio.currentTime = 0;
+            this._audio.play();
+
+        }
+
+    }
+
+    initKeyboard() {
+
+        document.addEventListener('keyup', e=> {
+
+            this.playAudio();
+
+            switch (e.key) {
+
+                case 'Escape':
+                    this.clearAll();
+                    break;
+    
+                case 'Backspace':
+                    this.clearEntry();
+                    break;
+    
+                case '+':    
+                case '-':    
+                case '*':    
+                case '/':    
+                case '%':
+                    this.addOperation(e.key);
+                    break;
+    
+                case 'Enter':
+                case '=':
+                    this.calc();
+                    break;
+    
+                case '.':
+                case ',':
+                    this.addDot();
+                    break;
+    
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    this.addOperation(parseInt(e.key));
+                    break;
+
+                case 'c':
+                    if (e.ctrlKey) this.copyToClipboard();
+                    break;
+    
+            }
+
+        })
 
     }
 
@@ -91,9 +207,13 @@ class CalcController {
     getResult(){
 
 
-
-        return eval(this._operation.join(""));
-
+        try{
+            return eval(this._operation.join(""));
+        } catch(e){
+            setTimeout(()=>{
+                this.setError();
+            }, 1);
+        }
     }
 
     calc(){
@@ -243,6 +363,8 @@ class CalcController {
 
     execBtn(value){
 
+        this.playAudio();
+
         switch (value) {
 
             case 'ac':
@@ -368,6 +490,10 @@ class CalcController {
     }
 
     set displayCalc(value){
+        if (value.toString().length > 10) {
+            this.setError();
+            return false;
+        }
 
         this._displayCalcEl.innerHTML = value;
 
